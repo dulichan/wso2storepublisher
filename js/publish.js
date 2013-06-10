4f75a5ecbc8f850a007c5859f9dcd546198d041c
Chatura Dilan <dilan@ws02mobile.com>
2013-06-10 11:58:41 +0530

    added publisher

---
+++ js/publish.js
@@ -0,0 +1,40 @@
+$('#application-tab a').click(function(e) {
+	e.preventDefault();
+	$(this).tab('show');
+});
+
+
+$('#btn-app-save').click(function(e) {
+	
+	var name = $("#txtName").val();
+	var description = $("#txtDescription").val();
+	var category = $("#txtCategory").val();
+	var recentChanges = $("#txtRecentChanges").val();
+	var banner = $("#txtbanner").val();
+	var screenShot1 = $("#txtScreenShot1").val();
+	var screenShot2 = $("#txtScreenShot2").val();
+	var screenShot3 = $("#txtScreenShot3").val();	
+	var iconfile = $("#txtIconfile").val();
+	var isMeetGudeLines = $("#chkMeetGudeLines").val();
+	
+	$.ajax({
+      type: "POST",
+      url: "apps/upload",
+      enctype: 'multipart/form-data',
+      data: {
+        name: name,
+        description: description,
+        category: category,
+        recentChanges: recentChanges,
+        banner: banner,
+        screenShot1: screenShot1,
+        screenShot2: screenShot2,
+        screenShot3: screenShot3,
+        iconfile: iconfile,
+        isMeetGudeLines: isMeetGudeLines
+      },
+      success: function () {
+        alert("Data Uploaded: ");
+      }
+    });
+});
